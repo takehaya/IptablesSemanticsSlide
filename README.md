0ac68e5f40ab2ae7b8200a27885911962efc888e
TakeruHayasaka <taketarou2@gmail.com>
2018-03-06 01:44:29 +0900

    setfiles

---
+++ README.md
@@ -0,0 +1,1 @@
+# iptables seamtincs slide
---
+++ src/webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+
+
+module.exports = {
+    entry:[
+        "./src/index"
+    ],
+    output: {
+        filename: "index.js"
+    },
+    resolve:{
+        extensions: [".tsx", ".ts", ".js", ".jpg", ".png"]
+    },
+    module: {
+        rules:[
+            {
+                test: /\.tsx$/,
+                use:[
+                    
+                ]
+            }
+        ]
+    }
+};
+// module.exports = {
+//     entry: [
+//         "react-hot-loader/patch",
+//         "./src/index",
+//     ],
+//     output: {
+//         filename: "index.js",
+//     },
+//     resolve: {
+//         extensions: [".tsx", ".ts", ".js", ".jpg", ".png"],
+//     },
+//     plugins: [
+//         new webpack.HotModuleReplacementPlugin(),
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.tsx?$/,
+//                 use: [
+//                     { loader: "react-hot-loader/webpack" },
+//                     { loader: "awesome-typescript-loader" },
+//                 ],
+//             },
+//             {
+//                 test: /\.css$/,
+//                 use: [
+//                     { loader: "style-loader" },
+//                     { loader: "raw-loader" }
+//                 ],
+//             },
+//             {
+//                 test: /\.(jpg|png|svg)$/,
+//                 loader: "url-loader",
+//             },
+//         ],
+//     },
+//     devServer: {
+//         hot: true,
+//         inline: true,
+//     },
+//     devtool: "eval",
+// };
